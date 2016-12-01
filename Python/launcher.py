f3d3a6d3e9d1300702487a0ee802420a814a79e3
Antonin <antonin.renaudier@gmail.com>
2016-12-01 17:14:43 +0100

    Bug negamax

--- Python/launcher.py
+++ Python/launcher.py
@@ -3,6 +3,6 @@ from human_player import HumanPlayer
 from newbie_player import NewbiePlayer
 from negamax_player import NegamaxPlayer
 
-game = Game(NegamaxPlayer(5), NegamaxPlayer(6))
+game = Game(NegamaxPlayer(4), NegamaxPlayer(5))  """ Bug pour une profondeur de 5 contre 6 et 4 contre 5, laisse des adversaires sans graines"""
 game.new_game()
 print(game.display_result())
