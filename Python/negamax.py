393815cae5984ee82fe60877b51122047ef9ca4c
Antonin <antonin.renaudier@gmail.com>
2016-12-01 16:15:13 +0100

    modif negamax

--- Python/negamax.py
+++ Python/negamax.py
@@ -1,5 +1,5 @@
-import numpy
 from awale import Awale
+
 
 class Minmax:
     """
@@ -7,21 +7,31 @@ class Minmax:
     """
 
     @staticmethod
-
-    def negamax(awale,depth,player):
-        if awale.game_over or depth==0:
-            return(awale.evaluation1(player))
+    def negamax(awale, depth, player, game_over):
+        if game_over or depth == 0:
+            return awale.evaluation1(player)
         else:
             best_score = float("inf")
             best_move = float("inf")
-            possible_move =[]
-            for i in range (6*player,6*(1+player)):
-                if awale.can_play(player,i):
+            possible_move = []
+            for i in range(6 * player, 6 * (1 + player)):
+                if awale.can_play(player, i):
                     possible_move.append(i)
             for j in possible_move:
                 new_board, new_score = awale.pick(player, j)
-                score = -Minmax.negamax(Awale(new_board,new_score),depth-1,player)
-                if score>=best_score:
-                    best_score=score
-                    best_move=j
-        return(best_move)
+                new_awale = Awale(new_board, new_score)
+                minmove = player * 6
+                maxmove = (1 + player) * 6
+                minpick = (1 - player) * 6
+                maxpick = (2 - player) * 6
+                cannot_feed = new_awale.board[minpick:maxpick].sum() == 0
+
+                for i in range(minmove, maxmove):
+                    cannot_feed = cannot_feed and new_awale.will_starve(player, i)
+
+                game_state = new_awale.score[player] >= 24 or new_awale.score[1 - player] >= 24 or cannot_feed
+                score = -Minmax.negamax(new_awale, depth - 1, player, game_state)
+                if score >= best_score:
+                    best_score = score
+                    best_move = j
+        return best_move
