19dc910a0f06197ce4b150e89c62d57dc53c1c77
Antonin <antonin.renaudier@gmail.com>
2016-12-01 16:22:07 +0100

    Test negamax

--- Python/negamax_player.py
+++ Python/negamax_player.py
@@ -1,6 +1,7 @@
 from negamax import Negamax
 
+
 class NegamaxPlayer:
     @staticmethod
-    def get_move(awale,depth,player):
-        return Negamax.negamax(awale,depth,player,False)
+    def get_move(awale, depth, player):
+        return Negamax.negamax(awale, depth, player, False)
