8e9141fe28018785bf85503be4e4f82e3a5fa9ed
Antonin <antonin.renaudier@gmail.com>
2016-12-01 16:01:49 +0100

    Add negamax

--- Python/negamax.py
+++ Python/negamax.py
@@ -1,9 +1,27 @@
 import numpy
 from awale import Awale
-from human_player import HumanPlayer
 
+class Minmax:
+    """
+    créer l'arbre et renvoie le trou optimal à jouer
+    """
 
-# def minmax(awale, depth, player):
-#     if awale.game_over or depth == 0:
-#         return eval1(awale, player)
-#     else:
+    @staticmethod
+
+    def negamax(awale,depth,player):
+        if awale.game_over or depth==0:
+            return(awale.evaluation1(player))
+        else:
+            best_score = float("inf")
+            best_move = float("inf")
+            possible_move =[]
+            for i in range (6*player,6*(1+player)):
+                if awale.can_play(player,i):
+                    possible_move.append(i)
+            for j in possible_move:
+                new_board, new_score = awale.pick(player, j)
+                score = -Minmax.negamax(Awale(new_board,new_score),depth-1,player)
+                if score>=best_score:
+                    best_score=score
+                    best_move=j
+        return(best_move)
