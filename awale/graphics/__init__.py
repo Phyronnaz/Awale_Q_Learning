4f309336748a2b4842cb9547628fbef88a888e20
Phyronnaz <phyronnaz@gmail.com>
2017-05-24 00:16:00 +0200

    Deletions

--- awale/graphics/__init__.py
+++
@@ -1,5 +0,0 @@
-from .hex_view import HexView
-from .mainwindow import Ui_TIPE
-from .ui import UI
-
-__all__ = ["UI"]
--- awale/graphics/debug.py
+++
@@ -1,5 +0,0 @@
-debug_play_text = None
-
-
-def debug_play(text):
-    debug_play_text.appendPlainText(text)
